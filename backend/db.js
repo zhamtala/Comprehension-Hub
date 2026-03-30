f1b03f9efee0da99de894f2c3682e0c9e2c369fb
Zham Tala <zhamtala@gmail.com>
2026-03-30 15:11:56 +0800

    switched to aiven mysql

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,14 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const db = mysql.createPool({
+const db = await mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
   ssl: {
     rejectUnauthorized: false,
   },
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
 });
 
 export default db;
