01ed96671c6b712d1f0b90bea90eb42a94f17ca5
Zham Tala <zhamtala@gmail.com>
2026-04-23 21:57:32 +0800

    student dashboard update 2

--- frontend/app/dashboards/StudentDashboard/page.tsx
+++ frontend/app/dashboards/StudentDashboard/page.tsx
@@ -34,7 +34,7 @@ export default function StudentDashboard() {
       return;
     }
 
-    fetch("${process.env.NEXT_PUBLIC_API_URL}/api/users", {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, {
       headers: { Authorization: `Bearer ${token}` },
     })
       .then((res) => {
